1440a73fdd2a9c0840f95a629b0bc0dbfc68919e
DorianNoam <dorian33270@hotmail.fr>
2026-08-06 12:13:32 +0200

    Delete src/app/pro/logout/route.ts

--- src/app/pro/logout/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export async function POST(request: Request) {
-  const response = NextResponse.redirect(new URL('/pro/login', request.url))
-  response.cookies.delete('bookme_pro_token')
-  return response
-}
