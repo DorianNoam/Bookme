3d0b36dda800e3a0619211f5cef55ab9c3423bea
DorianNoam <dorian33270@hotmail.fr>
2026-08-15 23:46:11 +0200

    Update route.ts

--- src/app/api/pro/auth/register/route.ts
+++ src/app/api/pro/auth/register/route.ts
@@ -1,3 +1,4 @@
+import { sendAdminNewProNotification } from '@/lib/email'
 import { sendProWelcome } from '@/lib/email'
 import { NextRequest, NextResponse } from 'next/server'
 import { createClient } from '@supabase/supabase-js'
