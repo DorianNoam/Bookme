1ca6ad93d91added29200e7c01945dd54b093dde
DorianNoam <dorian33270@hotmail.fr>
2026-08-16 15:33:28 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,27 @@ const nextConfig = {
       { protocol: 'https', hostname: '*.supabase.co' },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://maps.googleapis.com https://maps.google.com",
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+              "img-src 'self' data: blob: https: http:",
+              "font-src 'self' https://fonts.gstatic.com",
+              "connect-src 'self' https://*.supabase.co https://maps.googleapis.com https://nominatim.openstreetmap.org https://*.tile.openstreetmap.org",
+              "frame-src 'self' https://maps.googleapis.com https://maps.google.com",
+            ].join('; ')
+          }
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
